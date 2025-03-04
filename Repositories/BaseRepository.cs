3200f2f6d1b4e54d41c66564f150bef897795e17
4ndris1114 <93518856+4ndris1114@users.noreply.github.com>
2025-03-04 12:39:27 +0100

    made base repo class abstract + its methods virtual

--- Repositories/BaseRepository.cs
+++ Repositories/BaseRepository.cs
@@ -3,33 +3,37 @@ using MongoDB.Driver;
 
 namespace compulsoryRest.Repositories;
 
-public class BaseRepository<T> where T : class {
+public abstract class BaseRepository<T> where T : class {
     private readonly IMongoCollection<T> _collection;
 
     public BaseRepository(MongoDbContext dbContext, string collectionName) {
         _collection = dbContext.Database.GetCollection<T>(collectionName);
     }
 
-    public async Task<IEnumerable<T>> GetAllAsync() {
+    public virtual async Task<IEnumerable<T>> GetAllAsync() {
         return await _collection.Find(Builders<T>.Filter.Empty).ToListAsync();
     }
 
-    public async Task<T> GetByIdAsync(string id) {
+    public virtual async Task<T> GetByIdAsync(string id) {
         var filter = Builders<T>.Filter.Eq("Id", id);
         return await _collection.Find(filter).FirstOrDefaultAsync();
     }
 
-    public async Task CreateAsync(T entity) {
+    public virtual async Task CreateAsync(T entity) {
         await _collection.InsertOneAsync(entity);
     }
 
-    public async Task UpdateAsync(string id, T entity) {
+    public virtual async Task UpdateAsync(string id, T entity) {
         var filter = Builders<T>.Filter.Eq("Id", id);
         await _collection.ReplaceOneAsync(filter, entity);
     }
 
-    public async Task DeleteAsync(string id) {
+    public virtual async Task DeleteAsync(string id) {
         var filter = Builders<T>.Filter.Eq("Id", id);
         await _collection.DeleteOneAsync(filter);
+    }
+
+    public IMongoCollection<T> getCollection() {
+        return _collection;
     }
 }
--- Repositories/MovieRepository.cs
+++ Repositories/MovieRepository.cs
@@ -1,24 +1,21 @@
 using MongoDB.Driver;
 using compulsoryRest.Models;
-using compulsoryRest.Repositories;
 using compulsoryRest.Database;
 
 namespace compulsoryRest.Repositories;
 
-public class MovieRepository : BaseRepository<Movie> {
-    private readonly IMongoCollection<Movie> _movieCollection;
-
-    public MovieRepository(MongoDbContext dbContext) 
-        : base(dbContext, "Movies") {
-        _movieCollection = dbContext.Database.GetCollection<Movie>("Movies");
-    }
+public class MovieRepository : BaseRepository<Movie>
+{
+    public MovieRepository(MongoDbContext dbContext)
+        : base(dbContext, "Movies") {}
+        
     public async Task<IEnumerable<Movie>> GetMoviesByGenreAsync(string genre) {
         var filter = Builders<Movie>.Filter.Eq(m => m.Genre, genre);
-        return await _movieCollection.Find(filter).ToListAsync();
+        return await getCollection().Find(filter).ToListAsync();
     }
 
     public async Task<IEnumerable<Movie>> GetMoviesByYearAsync(int year) {
         var filter = Builders<Movie>.Filter.Eq(m => m.Year, year);
-        return await _movieCollection.Find(filter).ToListAsync();
+        return await getCollection().Find(filter).ToListAsync();
     }
 }

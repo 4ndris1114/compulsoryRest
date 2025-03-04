ae5e35c97031d70cb0fb59f65d7f26a933530913
4ndris1114 <93518856+4ndris1114@users.noreply.github.com>
2025-03-04 10:12:25 +0100

    refactored BaseController to an abstract class

--- Controllers/BaseController.cs
+++ Controllers/BaseController.cs
@@ -3,41 +3,27 @@ using compulsoryRest.Repositories;
 
 namespace compulsoryRest.Controllers;
 
-public class BaseController<T> : ControllerBase where T : class {
-    private readonly BaseRepository<T> _repository;
+[ApiController]
+[Route("api/[controller]")]
+public abstract class BaseController<T> : ControllerBase where T : class {
+    protected readonly BaseRepository<T> _repository;
 
     public BaseController(BaseRepository<T> repository) {
         _repository = repository;
     }
 
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<T>>> GetAll() {
-        var entities = await _repository.GetAllAsync();
-        return Ok(entities);
-    }
+    public abstract Task<ActionResult<IEnumerable<T>>> GetAll();
 
     [HttpGet("{id}")]
-    public async Task<ActionResult<T>> GetById(string id) {
-        var entity = await _repository.GetByIdAsync(id);
-        if (entity == null) return NotFound();
-        return Ok(entity);
-    }
+    public abstract Task<ActionResult<T>> GetById(string id);
 
     [HttpPost]
-    public async Task<ActionResult<T>> Create(T entity) {
-        await _repository.CreateAsync(entity);
-        return CreatedAtAction(nameof(GetById), new { id = entity.GetType().GetProperty("Id")?.GetValue(entity) }, entity);
-    }
+    public abstract Task<ActionResult<T>> Create(T entity);
 
     [HttpPut("{id}")]
-    public async Task<IActionResult> Update(string id, T entity) {
-        await _repository.UpdateAsync(id, entity);
-        return NoContent();
-    }
+    public abstract Task<IActionResult> Update(string id, T entity);
 
     [HttpDelete("{id}")]
-    public async Task<IActionResult> Delete(string id) {
-        await _repository.DeleteAsync(id);
-        return NoContent();
-    }
+    public abstract Task<IActionResult> Delete(string id);
 }

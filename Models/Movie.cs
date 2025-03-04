1fb10f00449c2d1b3c2c957dfc5ecfb05c4a316a
4ndris1114 <93518856+4ndris1114@users.noreply.github.com>
2025-03-04 11:25:02 +0100

    added validations

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -10,36 +10,36 @@ public class Movie {
     public string? Id { get; set;}
 
     [Required]
-    [StringLength(255, MinimumLength = 2)]
+    [StringLength(255, MinimumLength = 2, ErrorMessage = "Title must be between 2 and 255 characters")]
     [BsonElement("title")]
     public string Title { get; set; } = "N/A";
 
     [BsonElement("genre")]
+    [StringLength(50, MinimumLength = 2, ErrorMessage = "Genre must be between 2 and 50 characters")]
     public string? Genre { get; set; }
 
     [Required]
-    [Range(1900, 2030)]
+    [Range(1900, 2030, ErrorMessage = "Year must be between 1900 and 2030")]
     [BsonElement("year")]
     public int Year { get; set; }
 
     [Required]
-    [Range(0, 10)]
+    [Range(0, 10, ErrorMessage = "Rating must be between 0 and 10")]
     [BsonElement("rating")]
     public double Rating { get; set; } = double.NaN;
 
     [BsonElement("director")]
+    [StringLength(255, MinimumLength = 2, ErrorMessage = "Director must be between 2 and 255 characters")]
     public string? Director { get; set; }
 
     [Required]
-    [StringLength(1024, MinimumLength = 2)]
+    [StringLength(1024, MinimumLength = 2, ErrorMessage = "Description must be between 2 and 1024 characters")]
     [BsonElement("description")]
     public string Description { get; set; } = "N/A";
 
-    [Required]
     [BsonElement("createdAt")]
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
-    [Required]
     [BsonElement("updatedAt")]
     public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
 }
